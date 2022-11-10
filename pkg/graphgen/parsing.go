eff708a880da211cd93fa246d6bc5b51a790f3e7
roman <roman@arangodb.com>
2022-11-10 14:06:58 +0100

    $1

--- pkg/graphgen/parsing.go
+++ pkg/graphgen/parsing.go
@@ -8,150 +8,154 @@ import (
 	"reflect"
 )
 
+func unionString() string   { return "union" }
+func lexProdString() string { return "lexProduct" }
+func treeString() string    { return "tree" }
+func pathString() string    { return "path" }
+func cycleString() string   { return "cycle" }
+
+func contains(string *string, strings []string) bool {
+	return true
+}
+
+func checkSubtreeInput(jsonNodeName *string, subtree *interface{}) error {
+	switch *jsonNodeName {
+	case unionString(), lexProdString():
+		{
+			switch (*subtree).(type) {
+			case []interface{}:
+				{
+				}
+			default:
+				{
+					return errors.New(fmt.Sprintf("Error: Value of \"%s\" is not \"[]interface{}\" but %v",
+						*jsonNodeName, reflect.TypeOf(*subtree)))
+				}
+			}
+			numChildren := len((*subtree).([]interface{}))
+			if numChildren != 2 { // we assume that union also takes exactly 2 parameters!
+				return errors.New(fmt.Sprintf("Cannot parse the value of \"%s\": "+
+					"it should be a slice of length 2, "+
+					"however, it has length %d", *jsonNodeName, numChildren))
+			}
+			return nil
+		}
+
+	case pathString(), treeString(), cycleString():
+		{
+			switch (*subtree).(type) {
+			case map[string]interface{}:
+				{
+				}
+			default:
+				{
+					return errors.New(fmt.Sprintf(
+						"Error: value of \"%s\" is not \"map[string]interface{}\" but %v",
+						*jsonNodeName, reflect.TypeOf(*subtree)))
+				}
+			}
+			return nil
+		}
+	default:
+		{
+			return errors.New(fmt.Sprintf("Unknown node in json: %s", *jsonNodeName))
+		}
+	}
+
+}
+
+//func getLeftRight(subtree *interface)
+
 func parseJSONtoGraph(f map[string]any, prefix string) (*Generatable, error) {
-	for node, subtree := range f {
-		switch node {
+	if prefix != "" {
+		prefix += "_"
+	}
+	for jsonNodeName, subtree := range f {
+		err := checkSubtreeInput(&jsonNodeName, &subtree)
+		if err != nil {
+			return nil, err
+		}
+		switch jsonNodeName {
 		case "union":
 			{
-				switch subtree.(type) {
-				case []interface{}:
-					{
-						numChildren := len(subtree.([]interface{}))
-						if numChildren != 2 {
-							return nil,
-								errors.New(fmt.Sprintf("Cannot parse the value of \"union\": "+
-									"it should be a slice of length 2, "+
-									"however, it has length %d", numChildren))
-						}
-
-						if prefix != "" {
-							prefix += "_"
-						}
-
-						left := subtree.([]interface{})[0]
-						leftChild := left.(map[string]any)
-						proxyLeft, err := parseJSONtoGraph(leftChild, prefix+"a")
-						if err != nil {
-							return nil,
-								errors.New(fmt.Sprintf("Could not construct graph from %v",
-									leftChild))
-						}
-
-						right := subtree.([]interface{})[1]
-						rightChild := right.(map[string]any)
-						proxyRight, err := parseJSONtoGraph(rightChild, prefix+"b")
-						if err != nil {
-							return nil,
-								errors.New(fmt.Sprintf("Could not construct graph from %v",
-									rightChild))
-						}
-
-						var u Generatable = UnionParameters{*proxyLeft, *proxyRight, prefix}
-						return &u, nil
-
-					}
-				default:
+				left := subtree.([]interface{})[0]
+				leftChild := left.(map[string]any)
+				ggLeft, err := parseJSONtoGraph(leftChild, prefix+"a")
+				if err != nil {
 					return nil,
-						errors.New(fmt.Sprintf("Error: Value of \"union\" is not \"[]interface{}\" but %v",
-							reflect.TypeOf(subtree)))
+						errors.New(fmt.Sprintf("Could not construct graph from %v",
+							leftChild))
 				}
-				// return nil, nil
+
+				right := subtree.([]interface{})[1]
+				rightChild := right.(map[string]any)
+				ggRight, err := parseJSONtoGraph(rightChild, prefix+"b")
+				if err != nil {
+					return nil,
+						errors.New(fmt.Sprintf("Could not construct graph from %v",
+							rightChild))
+				}
+
+				var u Generatable = UnionParameters{*ggLeft, *ggRight, prefix}
+				return &u, nil
 			}
 		case "lexProduct":
 			{
-				switch subtree.(type) {
-				case []interface{}:
-					{
-						numChildren := len(subtree.([]interface{}))
-						if numChildren != 2 {
-							return nil,
-								errors.New(fmt.Sprintf("Cannot parse the value of \"lexProduct\": "+
-									"it should be a slice of length 2, "+
-									"however, it has length %d", numChildren))
-						}
-
-						if prefix != "" {
-							prefix += "_"
-						}
-
-						left := subtree.([]interface{})[0]
-						leftChild := left.(map[string]any)
-						proxyLeft, err := parseJSONtoGraph(leftChild, prefix+"a")
-						if err != nil {
-							return nil,
-								errors.New(fmt.Sprintf("Could not construct graph from %v",
-									leftChild))
-						}
-
-						right := subtree.([]interface{})[1]
-						rightChild := right.(map[string]any)
-						proxyRight, err := parseJSONtoGraph(rightChild, prefix+"b")
-						if err != nil {
-							return nil,
-								errors.New(fmt.Sprintf("Could not construct graph from %v",
-									rightChild))
-						}
-						var u Generatable = LexicographicalProductParameters{*proxyLeft, *proxyRight, prefix}
-						return &u, nil
-
-					}
-				default:
+				left := subtree.([]interface{})[0]
+				leftChild := left.(map[string]any)
+				ggLeft, err := parseJSONtoGraph(leftChild, prefix+"a")
+				if err != nil {
 					return nil,
-						errors.New(fmt.Sprintf("Error: Value of \"union\" is not \"[]interface{}\" but %v",
-							reflect.TypeOf(subtree)))
+						errors.New(fmt.Sprintf("Could not construct graph from %v",
+							leftChild))
 				}
-				// return nil, nil
+
+				right := subtree.([]interface{})[1]
+				rightChild := right.(map[string]any)
+				ggRight, err := parseJSONtoGraph(rightChild, prefix+"b")
+				if err != nil {
+					return nil,
+						errors.New(fmt.Sprintf("Could not construct graph from %v",
+							rightChild))
+				}
+				var lp Generatable = LexicographicalProductParameters{*ggLeft, *ggRight, prefix}
+				return &lp, nil
 			}
 
 		case "path":
 			{
-				switch subtree.(type) {
-				case map[string]interface{}:
-					length := uint64(subtree.(map[string]interface{})["length"].(float64)) // unmarshalling can only give float, never int
-					directed := subtree.(map[string]interface{})["directed"].(bool)
-					var path Generatable = &PathParameters{length, directed, prefix}
-					return &path, nil
-				default:
-					return nil, errors.New(fmt.Sprintf("Error: value of \"path\" is not \"map[string]interface{}\" but %v", reflect.TypeOf(subtree)))
-				}
+				length := uint64(subtree.(map[string]interface{})["length"].(float64)) // unmarshalling can only give float, never int
+				directed := subtree.(map[string]interface{})["directed"].(bool)
+				var path Generatable = &PathParameters{length, directed, prefix}
+				return &path, nil
 			}
 		case "tree":
 			{
-				switch subtree.(type) {
-				case map[string]interface{}:
-					branchingDegree := uint64(subtree.(map[string]interface{})["branchingDegree"].(float64))
-					depth := uint64(subtree.(map[string]interface{})["depth"].(float64))
-					directionType := subtree.(map[string]interface{})["directionType"].(string)
-					if directionType != "downwards" &&
-						directionType != "upwards" && directionType != "bidirected" {
-						return nil, errors.New(fmt.Sprintf(
-							"Error: value of \"tree.directionType\" shold "+
-								"be one of \"downwards\", \"upwards\", \"bidirected\", "+
-								", but it is %v", directionType))
-					}
-					var tree Generatable = &CompleteNaryTreeParameters{branchingDegree, depth, directionType, prefix}
-					return &tree, nil
-				default:
-					return nil, errors.New(fmt.Sprintf("Error: value of \"tree\" is not \"map[string]interface{}\" but %v", reflect.TypeOf(subtree)))
+				branchingDegree := uint64(subtree.(map[string]interface{})["branchingDegree"].(float64))
+				depth := uint64(subtree.(map[string]interface{})["depth"].(float64))
+				directionType := subtree.(map[string]interface{})["directionType"].(string)
+				if directionType != "downwards" &&
+					directionType != "upwards" && directionType != "bidirected" {
+					return nil, errors.New(fmt.Sprintf(
+						"Error: value of \"tree.directionType\" shold "+
+							"be one of \"downwards\", \"upwards\", \"bidirected\", "+
+							", but it is %v", directionType))
 				}
+				var tree Generatable = &CompleteNaryTreeParameters{branchingDegree, depth, directionType, prefix}
+				return &tree, nil
+
 			}
 		case "cycle":
 			{
-				switch subtree.(type) {
-				case map[string]interface{}:
-					length := uint64(subtree.(map[string]interface{})["length"].(float64))
-					var cycle Generatable = &CycleGraphParameters{length, prefix}
-					return &cycle, nil
-				default:
-					return nil, errors.New(fmt.Sprintf("Error: value of \"cycle\" is not \"map[string]interface{}\" but %v", reflect.TypeOf(subtree)))
-				}
+				length := uint64(subtree.(map[string]interface{})["length"].(float64))
+				var cycle Generatable = &CycleGraphParameters{length, prefix}
+				return &cycle, nil
 			}
 		default:
-			return nil, errors.New(fmt.Sprintf("Cannot parse %v", node))
+			return nil, errors.New(fmt.Sprintf("Cannot parse %v", jsonNodeName))
 		}
 
 	}
-	//
 	return nil, errors.New("We should not end up here. Probably forgot to return from the previous switch.")
 }
 
